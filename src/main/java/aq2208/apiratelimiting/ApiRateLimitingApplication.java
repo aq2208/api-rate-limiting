4f94712bd90c3d5196cfdbb77eb643781729367f
AnhQuan2208 <120321862+aq2208@users.noreply.github.com>
2024-01-14 23:34:01 +0700

    Initial commit

---
+++ src/main/java/aq2208/apiratelimiting/ApiRateLimitingApplication.java
@@ -0,0 +1,13 @@
+package aq2208.apiratelimiting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRateLimitingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRateLimitingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aq2208/apiratelimiting/ApiRateLimitingApplicationTests.java
@@ -0,0 +1,13 @@
+package aq2208.apiratelimiting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRateLimitingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
